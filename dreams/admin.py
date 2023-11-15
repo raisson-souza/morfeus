48fcb68043e723fdd4e32be20c1530dbfbd8a7a0
Raisson Souza <raissonrai@gmail.com>
2023-11-15 19:14:17 -0300

    Primeira implementação dos Sonhos

--- dreams/admin.py
+++ dreams/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from dreams.models import Dream
+
+class ListDreams(admin.ModelAdmin):
+    list_test = ['id', 'title', 'time']
+
+admin.site.register(Dream)
 
 # Register your models here.
--- dreams/models.py
+++ dreams/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+class Dream(models.Model):
+    """descrição"""
+
+    DREAM_TIME = (
+        ('DIA', 'Diurno'),
+        ('NOITE', 'Noturno'),
+        ('INDEFINIDO', 'Não presenciado')
+    )
+
+    # CAMPOS
+    title = models.CharField(
+        verbose_name="dream_title",
+        max_length=50,
+        help_text="Título do sonho",
+    )
+    time = models.CharField(
+        verbose_name="Estado do dia no sonho",
+        max_length=50,
+        default="INDEFINIDO",
+        help_text="dia / noite no sonho",
+        choices=DREAM_TIME
+    )
+
+    def __str__(self):
+        """String para representar o objeto Dream (no site Admin)."""
+        return f"Sonho ${ self.title }"
+
 # Create your models here.
