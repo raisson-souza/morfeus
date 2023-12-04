80cd9f4013b99ca17cbb25b79d5798d8f2d946eb
Raisson Souza <raissonrai@gmail.com>
2023-12-04 00:31:13 -0300

    Ajuste dos campos de cadastro de sonho.

--- dreams/forms.py
+++ dreams/forms.py
@@ -6,7 +6,7 @@ DREAM_TYPES = (
 )
 
 class DreamRegister(forms.Form):
-    title = forms.CharField(help_text="teste")
-    text = forms.CharField(widget=forms.Textarea)
-    dream_type = forms.ChoiceField(choices=DREAM_TYPES)
-    date = forms.DateField()
+    title = forms.CharField(label="Título do Sonho")
+    text = forms.CharField(widget=forms.Textarea, label="Descrição do Sonho")
+    dream_type = forms.ChoiceField(choices=DREAM_TYPES, label="Tipo de Sonho")
+    date = forms.DateField(label="Data do Sonho")
